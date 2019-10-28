19ef0ab2debd5256b47673b07210abff3969de31
Muhammad Juan Akbar <juan.akbar@heavenhr.de>
2019-10-28 16:24:17 +0100

    clean up

--- src/shared/middleware/webStorage/helpers.js
+++ src/shared/middleware/webStorage/helpers.js
@@ -2,33 +2,22 @@ const storage = localStorage;
 const AUTH_TOKEN_KEY = 'authToken';
 const CURRENT_USER_KEY = 'currentUser';
 
-export function setAuthToken(token) {
+export const setAuthToken = (token) => {
   storage.setItem(AUTH_TOKEN_KEY, token);
-}
+};
 
-export function getAuthToken() {
-  return storage.getItem(AUTH_TOKEN_KEY);
-}
+export const getAuthToken = () => storage.getItem(AUTH_TOKEN_KEY);
 
-export function clearAuthToken() {
-  storage.removeItem(AUTH_TOKEN_KEY);
-}
-
-export function setCurrentUser(user) {
+export const setCurrentUser = (user) => {
   const userObject = JSON.stringify(user);
   storage.setItem(CURRENT_USER_KEY, userObject);
-}
+};
 
-export function getCurrentUser() {
+export const getCurrentUser = () => {
   const userObject = storage.getItem(CURRENT_USER_KEY);
   return JSON.parse(userObject);
-}
+};
 
-export function clearCurrentUser() {
-  storage.removeItem(CURRENT_USER_KEY);
-}
-
-export function clearAll() {
-  clearCurrentUser();
-  clearAuthToken();
-}
+export const clearAll = () => {
+  storage.clear();
+};
